95d6dff58ed649c9adea24cfc8a6529dc696fd63
Marian Ludwig <marian.ludwig@carealytix.com>
2024-11-19 14:38:47 +0100

    Initial commit
    project cleanup

---
+++ App.tsx
@@ -0,0 +1,10 @@
+
+export const SAMPLE_API_URL =
+  'https://6374c09048dfab73a4e7b7c4.mockapi.io/logbook-configuration/1';
+
+// TODO: Get data from the sample API
+
+export default function App() {
+  // TODO: render results
+  return null;
+}
